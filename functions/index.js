8f574656d3a708f756fd699d19bb751cf1976f76
Dioceus <chuwei.chen2001@gmail.com>
2021-01-30 18:06:44 -0500

    initialize firebase

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,39 @@
 const functions = require('firebase-functions');
+const config = require("./util/config");
+const admin = require('firebase-admin');
+var serviceAccount = require("./util/serviceAccountKey.json");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: "https://cuthere-738da.firebaseio.com",
+  apiKey: "AIzaSyDG4ZutCpqflP4nK816YUIdHtiRZXYX_KM",
+    authDomain: "cuthere-738da.firebaseapp.com",
+    projectId: "cuthere-738da",
+    storageBucket: "cuthere-738da.appspot.com",
+    messagingSenderId: "154558297656",
+    appId: "1:154558297656:web:4c73f6c4663ff4d97a58cd",
+    measurementId: "G-LYL6N3BDZR"
+});
+
+//admin.initializeApp(config);
+
+const express = require("express"); 
+const app = express();
+app.use(express.json());
+
+const cors = require("cors");
+app.use(cors);
+
+const userRoute = require("./api/users/users");
+const coursesRoute = require("./api/courses/courses");
+
+app.use('/users', userRoute);
+app.use('/courses', coursesRoute);
+
+
+exports.api = functions.https.onRequest(app);
+/*
+app.listen(5001, ()=>{
+    console.log("App on port 5000")
+});
+*/
