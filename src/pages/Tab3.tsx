036e7738e0cba49a0c7b88a00943efb767787d0c
Siam Shafiq <siam.shafiq@gmail.com>
2021-01-30 23:07:54 -0800

    Implemented Clubs and Societies.

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,26 +1,124 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, {useState} from 'react';
+import { IonCardTitle, IonCardHeader,IonCardSubtitle,IonButtons,IonMenuToggle,IonListHeader,IonContent, IonHeader,IonCard,IonItemDivider,IonCheckbox,IonItem,IonMenu,IonList,IonRouterOutlet, IonIcon, IonLabel, IonButton, IonCardContent, IonPage, IonTitle, IonToolbar, IonApp, IonSplitPane } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import { INITIAL_EVENTS, createEventId } from '../event-utils'
 import './Tab3.css';
 
-const Tab3: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
-      </IonContent>
-    </IonPage>
-  );
-};
 
-export default Tab3;
+const checkboxList = [
+  { val: 'Pepperoni', isChecked: true },
+  { val: 'Sausage', isChecked: false },
+  { val: 'Mushroom', isChecked: false }
+];
+
+const Tab3: React.FC = () => {
+  return(
+  //   <div id="test">
+  //     <h1>Hello World</h1>
+  //   </div>
+
+  //   <div id = "test">
+  //     <h2></h2>
+  //   </div>
+  // )
+  // return React.createElement(
+  //   'div',
+  //   {id:'test', className:"dummyClass"},
+  //  )
+
+  
+  <IonApp>
+    <IonContent>
+    <IonSplitPane contentId="main" >
+    <IonMenu contentId="main" type="push" className="sidebar">
+
+    <IonHeader>
+      <IonToolbar color="primary" className="toolbar">
+        <IonTitle>Menu</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+
+    <IonContent>
+      <IonList>
+        <IonMenuToggle auto-hide="false">
+          <IonItem button>
+            <IonIcon slot="start" name='home'></IonIcon>
+            <IonLabel>
+              Home
+            </IonLabel>
+          </IonItem>
+        </IonMenuToggle>
+      </IonList>
+    </IonContent>
+  </IonMenu>
+  
+  
+
+  <IonPage className="ion-page" id="main">
+    <IonHeader>
+      <IonToolbar className = "headerClass">
+        <IonButtons slot="start">
+          <IonMenuToggle>
+            <IonButton>
+              <IonIcon slot="icon-only" name='menu'></IonIcon>
+            </IonButton>
+          </IonMenuToggle>
+        </IonButtons>
+        
+
+        <div className = "Buttons">
+          <IonButton>Settings</IonButton>
+          <IonButton>Settings</IonButton>
+          <IonButton>Settings</IonButton>
+        </div>
+      </IonToolbar>
+    </IonHeader>
+    
+    <IonContent className="cards">
+    <div className = "ionCards">
+    <IonTitle>Clubs and Societies</IonTitle>
+    <IonCard>
+        <IonCardHeader>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+          <IonCardTitle>Card Title</IonCardTitle>
+        </IonCardHeader>
+
+        <IonCardContent>
+          Keep close to Nature's heart... and break clear away, once in awhile,
+          and climb a mountain or spend a week in the woods. Wash your spirit clean.
+        </IonCardContent>
+    </IonCard>
+    <IonCard className = "cards">
+        <IonCardHeader>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+          <IonCardTitle>Card Title</IonCardTitle>
+        </IonCardHeader>
+
+        <IonCardContent>
+          Keep close to Nature's heart... and break clear away, once in awhile,
+          and climb a mountain or spend a week in the woods. Wash your spirit clean.
+        </IonCardContent>
+      </IonCard>
+      <IonCard  className = "cards">
+        <IonCardHeader>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+          <IonCardTitle>Card Title</IonCardTitle>
+        </IonCardHeader>
+
+        <IonCardContent>
+          Keep close to Nature's heart... and break clear away, once in awhile,
+          and climb a mountain or spend a week in the woods. Wash your spirit clean.
+        </IonCardContent>
+      </IonCard>
+      </div>
+      </IonContent>
+
+
+  </IonPage>
+  </IonSplitPane>
+  </IonContent>
+  </IonApp>
+  );
+}
+
+export default Tab3
